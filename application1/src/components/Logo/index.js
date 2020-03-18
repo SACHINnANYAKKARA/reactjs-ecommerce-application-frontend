e45c88c76232512aee4c0d7ddb8a477509dc5393
SACHINnANYAKKARA <sachinnanayakkara21@gmail.com>
2020-03-18 20:37:31 +0530

    commit

---
+++ application1/src/components/Logo/index.js
@@ -0,0 +1,14 @@
+import React from 'react';
+import './style.css';
+
+import { Link } from 'react-router-dom';
+
+const Logo = props => {
+    return (
+        <Link to="/">
+        </Link>
+        
+    );
+}
+
+export default Logo;
---
+++ application2.1/src/containers/App.js
@@ -0,0 +1,43 @@
+import React from 'react';
+import { BrowserRouter as Router, Switch, Route } from 'react-router-dom';
+import Shop from './Shop';
+
+import { Provider } from 'react-redux';
+import { createStore, combineReducers, applyMiddleware } from 'redux';
+import thunk from 'redux-thunk';
+import authReducers from '../store/reducers/authReducers';
+import productReducers from '../store/reducers/productReducers';
+import cartReducers from '../store/reducers/cartReducers';
+
+
+const rootReducers = combineReducers({
+  auth: authReducers,
+  products: productReducers,
+  cart: cartReducers
+});
+
+const store = createStore(rootReducers, applyMiddleware(thunk));
+
+window.store = store;
+
+function App() {
+  return (
+
+    <Provider store={store}>
+      <Router>
+        <div className="App">
+            <Switch>
+              
+              <Route path="/"  component={Shop} />
+              
+            </Switch>
+            
+          
+        </div>
+      </Router>
+    </Provider>
+    
+  );
+}
+
+export default App;
