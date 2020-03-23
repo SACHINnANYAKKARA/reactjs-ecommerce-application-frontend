0816c3521003a4cb2c3b2ff80fbca1cd5c4c479e
SACHINnANYAKKARA <sachinnanayakkara21@gmail.com>
2020-03-23 19:17:32 +0530

    commt

--- application2.1/src/containers/Products/Product/index.js
+++ application2.1/src/containers/Products/Product/index.js
@@ -1,24 +1,16 @@
 import React from 'react';
 import {Card} from 'antd';
 
-const layout = {
-    labelCol: {span: 8},
-    wrapperCol: {span: 16},
-};
-const tailLayout = {
-    wrapperCol: {offset: 8, span: 16},
-};
-
 const Product = props => {
 
     return (
-            <div className="Product">
-                <Card className="product-card-body"  style={{ width: 180 }}
-                      cover={<img alt="example" src={props.productImage} />}>
-                    <p>{props.name}</p>
-                    <p>${props.price}</p>
-                </Card>
-            </div>
+        <div className="Product">
+            <Card className="product-card-body" style={{width: 180}}
+                  cover={<img alt="example" src={props.productImage}/>}>
+                <p>{props.name}</p>
+                <p>${props.price}</p>
+            </Card>
+        </div>
     );
 }
 
